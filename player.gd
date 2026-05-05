470c160c15232f61db93d4995dbb3a1e232e1ffb
Levon Booth <boothlevon@gmail.com>
2026-05-05 15:59:12 +1200

    Initial commit

---
+++ player.gd
@@ -0,0 +1,14 @@
+extends CharacterBody2D
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -500.0
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+func _physics_process(delta):
+	if not is_on_floor():
+		velocity.y += gravity * delta
+	if Input.is_action_just_pressed("Jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+	var direction = Input.get_axis("Left", "Right")
+	velocity.x = direction * SPEED
+	move_and_slide()
